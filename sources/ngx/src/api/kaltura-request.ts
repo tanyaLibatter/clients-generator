dc76d1b1a7612947bd56e46edfd4dfef8250709f
Tanya Libatter <tanya.libatter@kaltura.com>
2018-03-19 11:30:07 +0200

    fix ott error strategy

--- sources/ngx/src/api/kaltura-request.ts
+++ sources/ngx/src/api/kaltura-request.ts
@@ -31,13 +31,16 @@ export abstract class KalturaRequest<T> extends KalturaRequestBase {
 
     private _unwrapResponse(response: any): any {
         if (kalturaClientConfig.response.nestedResponse) {
-            if (response && response.hasOwnProperty('result')) {
+          if (response && response.hasOwnProperty('result') && response['result'] && response['result'].hasOwnProperty('error')) {
+            return response.result.error;
+          }
+          else if (response && response.hasOwnProperty('result')) {
                 return response.result;
-            } else if (response && response.hasOwnProperty('error')) {
+            }
+            else if (response && response.hasOwnProperty('error')) {
                 return response.error;
             }
         }
-
         return response;
     }
 
